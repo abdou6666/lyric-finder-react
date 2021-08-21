8a6d9ee57530ef9f6f1984a8a7130a424e4f939d
abdou sfayhi <abdousfayhi12@gmail.com>
2021-08-21 12:12:47 +0100

    first commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark mb-5">
+            <span className=".navbar-brand.mb-0.h1.mx-auto">LyricFinder</span>
+             
+        </nav>
+    );
+}
